4ad751a7c02e4fad299073619a2b9204bdcf10f9
dothuthanh <55688858+dothuthanh@users.noreply.github.com>
2020-03-28 09:43:04 +0700

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,12 @@
+package packe;
+
+public class Test {
+    public static void main(String[] args) {
+        Point2D point = new Point2D();
+        System.out.println(point);
+        point = new Point2D(3.0f,4.0f);
+    Point3D poin = new Point3D(4.0f,2.0f,4.5f);
+        System.out.println(poin);
+    }
+
+}
